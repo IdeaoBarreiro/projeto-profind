22666da1de053ddd1f7f9928af1b9c4f21444ba8
Vitor Juvino <juvinovitor@gmail.com>
2024-04-15 19:03:34 -0300

    alteracao

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-      <h1 className="text-4xl font-bold">Sejam Bem-Vindos</h1>
+      <h1 className="text-4xl font-bold">Nao Sejam Bem-Vindos</h1>
       <hr />
       <nav className="mt-6">
         <Link className="text-blue-500" href= "/portal">Login</Link> ou {' '}
